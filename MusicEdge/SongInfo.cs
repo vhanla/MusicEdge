108768e17a553ebe3bd57796b52b2c885af8dcb2
vhanla <vhanla@gmail.com>
2019-05-29 12:41:41 -0500

    Renamed to MusicEdge

---
+++ MusicEdge/SongInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicEdge
+{
+    class SongInfo
+    {
+        public string Artist { get; set; }
+        public string Title { get; set; }
+        public string Album { get; set; }
+        public string CoverArt { get; set; }
+        public int Duration { get; set; }
+
+    }
+}
